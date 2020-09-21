f03a44f6f759f06bf2d0b27991a248e93a51a8ad
Renato Taffi <renato.taffi@gmail.com>
2020-09-21 19:20:19 +0200

    add orders

---
+++ src/app/orders.ts
@@ -0,0 +1,26 @@
+export const orders = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    date: '12/02/2020'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    date: '12/10/2020'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    date: '01/02/2019'
+  },
+  {
+    name: 'Phone big',
+    price: 999,
+    description: 'a very big phone',
+    date: '31/07/2020'
+  }
+];
